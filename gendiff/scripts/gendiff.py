705e3185f3310759eaeb60934b39173e78bdb5b9
veetors <veetor.s@gmail.com>
2020-04-13 16:15:19 +0300

    add format option

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,12 +8,12 @@ import argparse
 
 def main():
     """Run main script."""
-
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
 
-    args = parser.parse_args()
+    parser.parse_args()
 
 
 if __name__ == '__main__':
