f8023dc8cd4fe063bec73c802339d42ec0135180
veetors <veetor.s@gmail.com>
2020-04-16 12:57:44 +0300

    allow use package as library

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding:utf-8 -*-
 
 """CLI tool that shows differences between two configuration files."""
+
+from gendiff.diff import generate as generate_diff
+
+__all__ = (  # noqa: WPS410
+    'generate_diff',
+)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,21 +6,6 @@ from gendiff import diff
 from gendiff.parser import get_parser
 
 
-def generate_diff(filepath1, filepath2):
-    """Compare two files and generate diff string.
-
-    Parameters:
-        filepath1 (str): path to first file
-        filepath2 (str): path to second file
-
-    Returns:
-        str
-    """
-    print('generate_diff')
-
-    return diff.generate(filepath1, filepath2)
-
-
 def main():
     """Run main script."""
     parser = get_parser()
