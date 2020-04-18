6186db5a5e3a05ce8aa92c7389dcee42be4bdf52
veetors <veetor.s@gmail.com>
2020-04-18 23:16:38 +0300

    refactor function for build diff AST

--- gendiff/diff/stringify.py
+++ gendiff/diff/stringify.py
@@ -2,7 +2,31 @@
 
 """Module with functions for transform diff tree to string."""
 
-from gendiff.diff import constants
+from gendiff.diff.constants import (
+    NOT_CHANGED,
+    UPDATED,
+    REMOVED,
+    ADDED,
+    TYPE,
+    VALUE,
+)
+
+
+def get_indent(level, correction=0):
+    return ' ' * (4 * level - correction)
+
+
+def get_indent_(level, correction=0):  # correction???
+    return ' ' * (4 * level - 2 - correction)
+
+
+def get_output(item_key, item_value, indent, sign=None):
+    return '{indent}{sign} {key}: {value}\n'.format(
+        indent=indent,
+        sign=sign if sign else ' ',
+        key=item_key,
+        value=item_value,
+    )
 
 
 def normalize_value(arg):
@@ -19,45 +43,133 @@ def normalize_value(arg):
     return str(arg).lower()
 
 
-def diff_stringify(tree):
-    """Transform diff object to string.
-
-    Parameters:
-        tree (obj): diff object
-
-    Returns:
-        str
-    """
+def stringify_obj(target_obj, level=1):
     output = ''
-
-    for key, item_value in tree.items():
-        if item_value[constants.TYPE] == constants.NOT_CHANGED:
-            output += '    {k}: {v}\n'.format(
-                k=key,
-                v=normalize_value(item_value[constants.VALUE]),
+    for item_key, item_value in target_obj.items():
+        if isinstance(item_value, dict):
+            output += stringify_obj(item_value, level + 1)
+        else:
+            output += get_output(
+                item_key=item_key,
+                item_value=normalize_value(item_value),
+                indent=get_indent_(level),
             )
+            # output += '{spaces}{key}: {value}\n'.format(
+            #     spaces=get_indent(level),
+            #     key=key,
+            #     value=normalize_value(item_value),
+            # )
+    return '{{\n{output}{spaces}}}'.format(
+        output=output,
+        spaces=get_indent(level - 1),
+    )
 
-        if item_value[constants.TYPE] == constants.UPDATED:
-            added = '  + {k}: {v}\n'.format(
-                k=key,
-                v=normalize_value(item_value[constants.VALUE]),
-            )
-            removed = '  - {k}: {v}\n'.format(
-                k=key,
-                v=normalize_value(item_value['prev_value']),
-            )
-            output += added + removed
 
-        if item_value[constants.TYPE] == constants.REMOVED:
-            output += '  - {k}: {v}\n'.format(
-                k=key,
-                v=normalize_value(item_value[constants.VALUE]),
-            )
+def build_value(item_value, level):
+    if isinstance(item_value, dict):
+        return stringify_obj(item_value, level)
+    return normalize_value(item_value)
 
-        if item_value[constants.TYPE] == constants.ADDED:
-            output += '  + {k}: {v}\n'.format(
-                k=key,
-                v=normalize_value(item_value[constants.VALUE]),
-            )
 
-    return '{{\n{output}}}'.format(output=output)
+def make_not_changed_item(item_key, item_value, level):
+    return get_output(
+        item_key=item_key,
+        item_value=build_value(item_value[VALUE], level + 1),
+        indent=get_indent_(level),
+    )
+    # return '{spaces}{key}: {value}\n'.format(
+    #     spaces=get_indent(level),
+    #     key=item_key,
+    #     value=build_value(item_value[VALUE], level + 1),
+    # )
+
+
+def make_updated_item(item_key, item_value, level):
+    if 'children' in item_value:
+        return get_output(
+            item_key=item_key,
+            item_value=diff_stringify(item_value['children'], level + 1),
+            indent=get_indent_(level),
+        )
+        # return '{spaces}{key}: {value}\n'.format(
+        #     spaces=get_indent(level),
+        #     key=item_key,
+        #     value=diff_stringify(item_value['children'], level + 1)
+        # )
+
+    added = get_output(
+        item_key=item_key,
+        item_value=build_value(item_value[VALUE], level + 1),
+        indent=get_indent_(level),
+        sign='+',
+    )
+    # added = '{spaces}+ {key}: {value}\n'.format(
+    #     spaces=get_indent(level, 2),
+    #     key=item_key,
+    #     value=build_value(item_value[VALUE], level + 1),
+    # )
+    removed = get_output(
+        item_key=item_key,
+        item_value=build_value(item_value['prev_value'], level + 1),
+        indent=get_indent_(level),
+        sign='-',
+    )
+    # removed = '{spaces}- {key}: {value}\n'.format(
+    #     spaces=get_indent(level, 2),
+    #     key=item_key,
+    #     value=build_value(item_value['prev_value'], level + 1),
+    # )
+    return added + removed
+
+
+def make_removed_item(item_key, item_value, level):
+    return get_output(
+        item_key=item_key,
+        item_value=build_value(item_value[VALUE], level + 1),
+        indent=get_indent_(level),
+        sign='-',
+    )
+    # return '{spaces}- {key}: {value}\n'.format(
+    #     spaces=get_indent(level, 2),
+    #     key=item_key,
+    #     value=build_value(item_value[VALUE], level + 1),
+    # )
+
+
+def make_added_item(item_key, item_value, level):
+    return get_output(
+        item_key=item_key,
+        item_value=build_value(item_value[VALUE], level + 1),
+        indent=get_indent_(level),
+        sign='+',
+    )
+    # return '{spaces}+ {key}: {value}\n'.format(
+    #     spaces=get_indent(level, 2),
+    #     key=item_key,
+    #     value=build_value(item_value[VALUE], level + 1),
+    # )
+
+
+items_actions = {
+    NOT_CHANGED: make_not_changed_item,
+    UPDATED: make_updated_item,
+    REMOVED: make_removed_item,
+    ADDED: make_added_item,
+}
+
+
+def diff_stringify(tree, level=1):
+    output = ''
+    keys = list(tree.keys())
+    keys.sort()
+
+    for key in keys:
+        item_value = tree[key]
+
+        make_item_func = items_actions[item_value[TYPE]]
+        output += make_item_func(key, item_value, level)
+
+    return '{{\n{output}{s}}}'.format(
+        s=get_indent(level - 1),
+        output=output,
+    )
--- gendiff/diff/tree.py
+++ gendiff/diff/tree.py
@@ -1,97 +1,96 @@
 # -*- coding:utf-8 -*-
 
-"""Module with functions for generate diff tree."""
+"""Module with functions for generate diff AST."""
 
-from gendiff.diff import constants
-
-
-def get_not_changed_items(config1, config2):
-    """Compare two configs and generate diff for not changed keys.
-
-    Parameters:
-        config1 (obj): first config
-        config2 (obj): second config
-
-    Returns:
-        obj
-    """
-    keys1 = set(config1.keys())
-    keys2 = set(config2.keys())
-    not_changed_items = {}
-    for key in keys1 & keys2:
-        if config1[key] != config2[key]:
-            continue
-        not_changed_items[key] = {
-            constants.TYPE: constants.NOT_CHANGED,
-            constants.VALUE: config1[key],
-        }
-    return not_changed_items
+from gendiff.diff.constants import (
+    ADDED,
+    NOT_CHANGED,
+    REMOVED,
+    TYPE,
+    UPDATED,
+    VALUE,
+)
 
 
-def get_updated_items(config1, config2):
-    """Compare two configs and generate diff for updated keys.
+def make_removed_item(item_value):
+    """Build AST-node for removed items.
 
     Parameters:
-        config1 (obj): first config
-        config2 (obj): second config
+        item_value (any): item value
 
     Returns:
-        obj
+        dict
     """
-    keys1 = set(config1.keys())
-    keys2 = set(config2.keys())
-    updated_items = {}
-    for key in keys1 & keys2:
-        if config1[key] == config2[key]:
-            continue
-        updated_items[key] = {
-            constants.TYPE: constants.UPDATED,
-            constants.VALUE: config2[key],
-            'prev_value': config1[key],
-        }
-    return updated_items
+    return {
+        TYPE: REMOVED,
+        VALUE: item_value,
+    }
 
 
-def get_removed_items(config1, config2):
-    """Compare two configs and generate diff for removed keys.
+def make_added_item(item_value):
+    """Build AST-node for added items.
 
     Parameters:
-        config1 (obj): first config
-        config2 (obj): second config
+        item_value (any): item value
 
     Returns:
-        obj
+        dict
     """
-    keys1 = set(config1.keys())
-    keys2 = set(config2.keys())
-    removed_items = {}
-    for key in keys1 - keys2:
-        removed_items[key] = {
-            constants.TYPE: constants.REMOVED,
-            constants.VALUE: config1[key],
-        }
-    return removed_items
+    return {
+        TYPE: ADDED,
+        VALUE: item_value,
+    }
 
 
-def get_added_itmes(config1, config2):
-    """Compare two configs and generate diff for added keys.
+def make_not_changed_item(item_value):
+    """Build AST-node for not changed items.
 
     Parameters:
-        config1 (obj): first config
-        config2 (obj): second config
+        item_value (any): item value
 
     Returns:
-        obj
+        dict
     """
-    keys1 = set(config1.keys())
-    keys2 = set(config2.keys())
-    added_items = {}
-    for key in keys2 - keys1:
-        added_items[key] = {
-            constants.TYPE: constants.ADDED,
-            constants.VALUE: config2[key],
+    return {
+        TYPE: NOT_CHANGED,
+        VALUE: item_value,
+    }
+
+
+def make_updated_item(item_value1, item_value2):
+    """Build AST-node for updated items.
+
+    Parameters:
+        item_value (any): item value
+
+    Returns:
+        dict
+    """
+    if isinstance(item_value1, dict) and isinstance(item_value2, dict):
+        return {
+            TYPE: UPDATED,
+            'children': get_diff_tree(item_value1, item_value2),
         }
-    return added_items
+    return {
+        TYPE: UPDATED,
+        VALUE: item_value2,
+        'prev_value': item_value1,
+    }
+
+
+def make_shared_item(item_value1, item_value2):
+    """Build AST-node for items with are in both configs.
+
+    Parameters:
+        item_value1 (any): first item value
+        item_value2 (any): second item value
+
+    Returns:
+        call function
+    """
+    if item_value1 == item_value2:
+        return make_not_changed_item(item_value1)
+    return make_updated_item(item_value1, item_value2)
 
 
 def get_diff_tree(config1, config2):
@@ -102,11 +101,18 @@ def get_diff_tree(config1, config2):
         config2 (obj): second config
 
     Returns:
-        obj
+        dict: diff AST
     """
-    return {
-        **get_not_changed_items(config1, config2),
-        **get_updated_items(config1, config2),
-        **get_removed_items(config1, config2),
-        **get_added_itmes(config1, config2),
-    }
+    tree = {}
+    keys1 = set(config1.keys())
+    keys2 = set(config2.keys())
+
+    for key in keys1 | keys2:
+        if key in keys1 & keys2:
+            tree[key] = make_shared_item(config1[key], config2[key])
+        if key in keys1 - keys2:
+            tree[key] = make_removed_item(config1[key])
+        if key in keys2 - keys1:
+            tree[key] = make_added_item(config2[key])
+
+    return tree
