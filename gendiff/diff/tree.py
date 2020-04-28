a6c46b2b185b8e52e7dbfa51cdd6a1b769fac317
veetors <veetor.s@gmail.com>
2020-04-28 11:17:30 +0300

    refactor tree.py

--- gendiff/diff/tree.py
+++ gendiff/diff/tree.py
@@ -2,98 +2,46 @@
 
 """Module with functions for generate diff AST."""
 
-from gendiff.diff.constants import (
-    ADDED,
-    CHILDREN,
-    NOT_CHANGED,
-    PARENT,
-    PREV_VALUE,
-    REMOVED,
-    TYPE,
-    UPDATED,
-    VALUE,
-)
+from gendiff.diff import constants
 
 
-def make_removed_node(item_value):
-    """Build AST-node for removed items.
-
-    Parameters:
-        item_value (any): item value
-
-    Returns:
-        dict
-    """
+def make_removed_node(item_value):  # noqa: D103
     return {
-        TYPE: REMOVED,
-        VALUE: item_value,
+        constants.TYPE: constants.REMOVED,
+        constants.VALUE: item_value,
     }
 
 
-def make_added_node(item_value):
-    """Build AST-node for added items.
-
-    Parameters:
-        item_value (any): item value
-
-    Returns:
-        dict
-    """
+def make_added_node(item_value):  # noqa: D103
     return {
-        TYPE: ADDED,
-        VALUE: item_value,
+        constants.TYPE: constants.ADDED,
+        constants.VALUE: item_value,
     }
 
 
-def make_not_changed_node(item_value):
-    """Build AST-node for not changed items.
-
-    Parameters:
-        item_value (any): item value
-
-    Returns:
-        dict
-    """
+def make_parent_node(item_value1, item_value2):  # noqa: D103
     return {
-        TYPE: NOT_CHANGED,
-        VALUE: item_value,
+        constants.TYPE: constants.PARENT,
+        constants.CHILDREN: make_diff_tree(item_value1, item_value2),
     }
 
 
-def make_updated_node(item_value1, item_value2):
-    """Build AST-node for updated items.
-
-    Parameters:
-        item_value1 (any): first item value
-        item_value2 (any): second item value
-
-    Returns:
-        dict
-    """
+def make_not_changed_node(item_value):  # noqa: D103
     return {
-        TYPE: UPDATED,
-        VALUE: item_value2,
-        PREV_VALUE: item_value1,
+        constants.TYPE: constants.NOT_CHANGED,
+        constants.VALUE: item_value,
     }
 
 
-def make_parent_node(item_value1, item_value2):
-    """Build AST-node for items that have child items.
-
-    Parameters:
-        item_value1 (any): first item value
-        item_value2 (any): second item value
-
-    Returns:
-        dict
-    """
+def make_updated_node(item_value1, item_value2):  # noqa: D103
     return {
-        TYPE: PARENT,
-        CHILDREN: make_diff_tree(item_value1, item_value2),
+        constants.TYPE: constants.UPDATED,
+        constants.VALUE: item_value2,
+        constants.PREV_VALUE: item_value1,
     }
 
 
-def make_diff_tree(config1, config2):
+def make_diff_tree(config1, config2):  # noqa: WPS210
     """Compare two configs and generate diff object.
 
     Parameters:
@@ -107,20 +55,21 @@ def make_diff_tree(config1, config2):
     keys1 = set(config1.keys())
     keys2 = set(config2.keys())
 
-    for key in keys1 | keys2:
-        if key in keys1 & keys2:
-            if config1[key] == config2[key]:
-                tree[key] = make_not_changed_node(config1[key])
-            else:
-                tree[key] = make_updated_node(config1[key], config2[key])
+    for removed_key in keys1 - keys2:
+        tree[removed_key] = make_removed_node(config1.get(removed_key))
 
-            if isinstance(config1[key], dict) and isinstance(config2[key], dict):
-                tree[key] = make_parent_node(config1[key], config2[key])
+    for added_key in keys2 - keys1:
+        tree[added_key] = make_added_node(config2.get(added_key))
 
-        if key in keys1 - keys2:
-            tree[key] = make_removed_node(config1[key])
+    for shared_key in keys1 & keys2:
+        item_value1 = config1.get(shared_key)
+        item_value2 = config2.get(shared_key)
 
-        if key in keys2 - keys1:
-            tree[key] = make_added_node(config2[key])
+        if isinstance(item_value1, dict) and isinstance(item_value2, dict):
+            tree[shared_key] = make_parent_node(item_value1, item_value2)
+        elif item_value1 == item_value2:
+            tree[shared_key] = make_not_changed_node(item_value2)
+        else:
+            tree[shared_key] = make_updated_node(item_value1, item_value2)
 
     return tree
