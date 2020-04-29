ef0f2a103c695b6b73e68b96ef4fb7f920c9b568
veetors <veetor.s@gmail.com>
2020-04-29 12:44:56 +0300

    check --format arg

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,13 +7,24 @@ import argparse
 
 from gendiff import diff
 
+DEFAULT_OUTPUT_FORMAT = 'nested'
+
 
 def main():
     """Run main script."""
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument(
+        '-f',
+        '--format',
+        type=str,
+        default=DEFAULT_OUTPUT_FORMAT,
+        choices=['nested', 'plain', 'json'],
+        help='set format of output (default: {default})'.format(
+            default=DEFAULT_OUTPUT_FORMAT,
+        ),
+    )
     args = parser.parse_args()
     output = diff.generate(args.first_file, args.second_file, args.format)
     print(output)
